4d19b27b0d27da40d37d89a6a2631149af7db871
Chetan Bhasin <connect@chetanbhasin.com>
2024-08-06 23:12:57 +0200

    Full system upgrade

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Chetan Bhasin's Nix configuration";
   inputs = {
     # Package sets
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
 
     # Environment/system management
     darwin = {
@@ -10,32 +10,28 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Other sources
     flake-utils = { url = "github:numtide/flake-utils"; };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     devshell = {
       url = "github:numtide/devshell";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
-    snm = {
-      url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
     };
 
   };
 
-  outputs = inputs@{ self, nixpkgs, darwin, home-manager, flake-utils, devshell
-    , snm, ... }:
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , darwin
+    , home-manager
+    , flake-utils
+    , devshell
+    , ...
+    }:
     let
       nixpkgsConfig = with inputs; {
         config = { allowUnfree = true; };
@@ -80,7 +76,8 @@
             };
           }
         ];
-    in {
+    in
+    {
       darwinConfigurations = {
         hugh = darwin.lib.darwinSystem {
           system = "aarch64-darwin";
@@ -148,6 +145,8 @@
             }
           ];
           networking.hostName = "${name}";
+          trusted-public-keys = [
+          ];
           users.users.root.openssh.authorizedKeys.keys = [
             # chetan's mac key
             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC8y+WOiiqxKGRQHGdtRGL2R4Ptqs7uEXX89WwvUQTc9A2zTFjGNcQvDCP6+qw6FQgDCaLdNozojfPQxo/VqMiWf1KXvBOPMONc+AUURhPxw8lD1FSc5AsLAw68BrxnFLbYrKmJT6qr3Ap/D6NGNlJUN3mR/e8Bj2wpKNSidmn9aDBxuGLkBmYJ8K8Wdalg47WwQ7wvzxCn4MFjM8CINyaI3p0mouZdCeCd/JcJgeLqN1JGuHCdgwzS9FgAWwQ0s/zb33icxS3qlHYLOch8YpD1wCceHJEv8dRQxwoEbdho9VwUzZGE8y2YPLxNLShSjUEPK5rLbfz4kUrWZCEX0LHhwyBKW0u8O7RArCKVDjJkiVEWoIrTmYx3CxppYnuyKPe85vUwqQzafN1EVvtfwQcJHBknG/9Fo5sU+juuTMIbFHNwFjBH4MzOnIRBAV2lGy4YsGZwE/+HVB9kFqZf3KrBeRSZsNMUxC0AXapOHKimHyUyHS/bJUH3onqPV1cD8/k= chetan@Chetans-Air"
