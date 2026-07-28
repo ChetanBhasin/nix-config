67dde31e09f35468b5ecdd3b5b8f825bc8ef70ab
Chetan Bhasin <git@mail.chetanbhasin.com>
2026-07-28 17:55:18 +0200

    chore: remove venus

--- hosts/venus/home.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  imports = [ ../../home ];
-  home-config-manager = {
-    includeFonts = true;
-    isDarwin = false;
-    enableExtras = true;
-    enableProf = true;
-  };
-}
